4218e5ebabf057430a764835680ddd0cbe1a74ca
Nam <nam.le2@ntq-solution.com.vn>
2021-11-09 13:45:54 +0700

    socket

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const app = express();
-const helmet = require("helmet");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 require("dotenv").config();
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  /* options */
+});
+
+io.on("connect", (socket) => {
+  log("socket on", socket);
+});
 
 const userRouter = require("./routes/users");
 const authRouter = require("./routes/auths");
@@ -13,6 +24,7 @@ const reportRouter = require("./routes/report");
 const conversationRouter = require("./routes/conversation");
 const messageRouter = require("./routes/message");
 const mailRouter = require("./routes/mail");
+
 mongoose.connect(process.env.MONGODB_URL, () => {
   console.log("connect db success");
 });
@@ -27,8 +39,9 @@ app.use("/api/savepost", savePostRouter);
 app.use("/api/report", reportRouter);
 app.use("/api/conversation", conversationRouter);
 app.use("/api/message", messageRouter);
-app.use("/api/email",mailRouter); 
+app.use("/api/email", mailRouter);
+
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Sever Running at ${PORT}`);
 });
