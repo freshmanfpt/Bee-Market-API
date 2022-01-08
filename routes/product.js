25f94914ac2210a98a55d1f69bb0d2640e67ae89
fptfreshman <thanhptph12015@fpt.edu.vn>
2022-01-08 20:15:25 +0700

    Fix :Product update , get product by id

--- routes/product.js
+++ routes/product.js
@@ -44,7 +44,7 @@ router.post("/", async (req, res) => {
 //Update Product
 router.put("/:id", async (req, res) => {
   try {
-    const Product = await Product.findByIdAndUpdate(req.params.id, {
+    const product = await Product.findByIdAndUpdate(req.params.id, {
       $set: req.body,
     });
     res.status(200).json("Product has been updated");
@@ -65,9 +65,10 @@ router.delete("/:id", async (req, res) => {
 
 //Get a Product
 router.get("/:id", async (req, res) => {
+  console.log(req.params.id);
   try {
-    const Product = await Product.findById(req.params.id);
-    res.status(200).json(Product);
+    const product = await Product.findById(req.params.id);
+    res.status(200).json(product);
   } catch (err) {
     res.status(500).json(err);
   }
